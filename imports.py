755a4fead770df30a45cfe4b8d3ff7d459a2b1af
Fornelos <thiagofornelos@gmail.com>
2026-01-07 21:27:58 -0300

    Imports

---
+++ imports.py
@@ -0,0 +1,10 @@
+import bisect
+import re
+
+from matplotlib import pyplot as plt
+import numpy as np
+import pandas as pd
+import scipy
+from scipy.special import comb, factorial
+
+from empiricaldist import FreqTab, Pmf, Cdf, Hazard
